a5fd944e2fabb27c31cf63c9564e96bfb2b53af1
yves <ysk@wizards-technologies.com>
2023-02-21 18:17:35 +0100

    tp routing

---
+++ www/controllers/Front.php
@@ -0,0 +1,12 @@
+<?php
+
+class Front{
+
+
+    public function home()
+    {
+        die("Accueil");
+    }
+
+
+}
--- www/index.php
+++ www/index.php
@@ -1,1 +1,15 @@
-Welcome
+<?php
+$uri = strtolower(trim($_SERVER["REQUEST_URI"], "/"));
+
+if(!file_exists("routes.yml")){
+    die("Le fichier routes.yml n'existe pas");
+}
+$routes  = yaml_parse_file("routes.yml");
+
+//Si l'uri n'existe pas dans $routes die page 404
+//Sinon si l'uri ne possède pas de controller ni d'action die erreur fichier routes.yml
+//Sinon si il n'y a pas de fichier controller correspondant die absence du fichier controller
+//Sinon si l'action n'existe pas die action inexistante
+//Sinon appel de l'action
+
+
